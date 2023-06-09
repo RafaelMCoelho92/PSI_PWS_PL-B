73de6d85ed4d9491b92e358a7a8488e2a7e9e0e1
Patricksoares100 <2220905@my.ipleiria.pt>
2023-06-09 12:04:44 +0100

    Iva Modelo

--- weblogicmvc/controllers/AuthController.php
+++ weblogicmvc/controllers/AuthController.php
@@ -1,18 +1,21 @@
 <?php  //Controlador de Login
 require_once 'models/Auth.php';
 require_once 'Controller.php';
+
+// AuthController herda todas as propriedades e métodos da classe Controller
 class AuthController extends Controller
 {
     public function index()
     {
-        $this->renderView('home', 'index');  
+        // vai mostrar a view sempre que for chamada
+        $this->renderView('home', 'index');
     }
     public function login()
     {
         $username = $_POST['username'];
         $password = $_POST['password'];
         $auth = new Auth();
-        if ($auth->checkAuth($username, $password) == true) {//e seja cliente ,func, admin manda pra sitios diferentes
+        if ($auth->checkAuth($username, $password) == true) { //e seja cliente ,func, admin manda pra sitios diferentes
 
             header('Location: index.php?c=layout&a=backoffice'); //redirectroroute??
         } else {
