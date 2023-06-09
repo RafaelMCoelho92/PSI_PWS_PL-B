53d20abd0228d3d3bdcbf456392fc7c84088424a
Patricksoares100 <2220905@my.ipleiria.pt>
2023-06-09 14:35:09 +0100

    iva index e ligação botão (com erro ao abrir)

---
+++ weblogicmvc/views/iva/index.php
@@ -0,0 +1,23 @@
+<h1 class="text-left top-space">Contatos <?= APP_NAME ?></h1>
+<h2 class="top-space"></h2>
+<div class="row">
+    <div class="col-sm-12">
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>
+                        <h3>IVA</h3>
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($ivas as $iva) { ?>
+                    <tr>
+                        <td><?= $iva->taxa ?></td>
+                        <!-- Adicione outras colunas da tabela aqui -->
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    </div>
+</div>
