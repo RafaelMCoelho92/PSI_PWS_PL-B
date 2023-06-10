6412902ddaf4295d596e79e5c1067ba7e7e3b999
Patricksoares100 <2220905@my.ipleiria.pt>
2023-06-10 23:52:22 +0100

    tentativa de dar IVA - index

--- weblogicmvc/controllers/IvaController.php
+++ weblogicmvc/controllers/IvaController.php
@@ -7,75 +7,61 @@ class IvaController extends Controller
     public function index()
     {
         $ivas = IVA::all();
-        // Mostrar a vista index passando os dados por parâmetro
         $this->renderView('iva', 'index', ['ivas' => $ivas]);
     }
+
     public function show($id)
     {
-        // Mostrar vista com detalhes
         $iva = IVA::find($id);
         if (is_null($iva)) {
-            // TODO: Redirecionar para página de erro padrão
+            // Redirecionar ou exibir mensagem de erro
         } else {
-            // Mostrar a vista show passando os dados por parâmetro
             $this->renderView('iva', 'show', ['iva' => $iva]);
         }
     }
 
     public function create()
     {
-        // Mostrar vista com formulário de criação de registo
         $this->renderView('iva', 'create');
     }
 
     public function store()
     {
-        // Receber os dados do formulário de criação, validar e persistir no BD
         $iva = new IVA($this->getHTTPPost());
         if ($iva->is_valid()) {
             $iva->save();
-            // Redirecionar para o index das IVA
             $this->redirectToRoute('iva', 'index');
         } else {
-            // Mostrar vista create passando o modelo como parâmetro
             $this->renderView('iva', 'create', ['iva' => $iva]);
         }
     }
 
     public function edit($id)
     {
-        // Mostrar a vista com formulário de edição de um registo identificado pelo seu ID
         $iva = IVA::find($id);
         if (is_null($iva)) {
-            // TODO: Redirecionar para página de erro padrão
-            $this->renderView('iva', 'index', ['iva' => $iva]);
+            // Redirecionar ou exibir mensagem de erro
         } else {
-            // Mostrar a vista edit passando os dados por parâmetro
             $this->renderView('iva', 'edit', ['iva' => $iva]);
         }
     }
 
     public function update($id)
     {
-        // Receber os dados do formulário de edição de um registo identificado pelo seu ID, validar e persistir no BD
         $iva = IVA::find($id);
         $iva->update_attributes($this->getHTTPPost());
         if ($iva->is_valid()) {
             $iva->save();
-            // Redirecionar para o index
             $this->redirectToRoute('iva', 'index');
         } else {
-            // Mostrar vista edit passando o modelo como parâmetro
             $this->renderView('iva', 'edit', ['iva' => $iva]);
         }
     }
 
     public function delete($id)
     {
-        // Apagar um registo do BD identificado pelo ID
         $iva = IVA::find($id);
         $iva->delete();
-        // Redirecionar para o index
         $this->redirectToRoute('iva', 'index');
     }
 }
--- weblogicmvc/views/iva/index.php
+++ weblogicmvc/views/iva/index.php
@@ -4,32 +4,33 @@
     <div class="col-sm-12">
         <table class="table table-striped">
             <thead>
-                <th>
-                    <h3>IVA em Vigor</h3>
-                </th>
-                <th>
-                    <h3>Descrição</h3>
-                </th>
-                <th>
-                    <h3>Percentagem</h3>
-                </th>
-                <th>
-                    <a href='index.php?c=iva&a=create' class="btn btn-success">Criar IVA</a>
-                </th>
+                <tr>
+                    <th>
+                        <h3>IVA em Vigor</h3>
+                    </th>
+                    <th>
+                        <h3>Descrição</h3>
+                    </th>
+                    <th>
+                        <h3>Percentagem</h3>
+                    </th>
+                    <th><a href="index.php?c=iva&a=create" class="btn btn-success">Criar IVA</a></th>
+                </tr>
             </thead>
             <tbody>
                 <?php foreach ($ivas as $iva) { ?>
                     <tr>
-                        <td><?= $iva->ivaVigor ?></td>
-                        <td><?= $iva->descricao ?></td>
-                        <td><?= $iva->percentagem ?></td>
+                        <td><?php echo $iva->ivaVigor; ?></td>
+                        <td><?php echo $iva->descricao; ?></td>
+                        <td><?php echo $iva->percentagem; ?></td>
                         <td>
-                            <a href="index.php?c=iva&a=show&id=<?= $iva->id ?>" class="btn btn-primary" role="button">Ver mais</a>
-                            <a href="index.php?c=iva&a=edit&id=<?= $iva->id ?>" class="btn btn-secondary" role="button">Editar</a>
-                            <a href="index.php?c=iva&a=delete&id=<?= $iva->id ?>" class="btn btn-danger" role="button">Delete</a>
+                            <a href="index.php?c=iva&a=show&id=<?php echo $iva->id; ?>" class="btn btn-primary" role="button">Ver mais</a>
+                            <a href="index.php?c=iva&a=edit&id=<?php echo $iva->id; ?>" class="btn btn-secondary" role="button">Editar</a>
+                            <a href="index.php?c=iva&a=delete&id=<?php echo $iva->id; ?>" class="btn btn-danger" role="button">Delete</a>
                         </td>
                     </tr>
                 <?php } ?>
+
             </tbody>
         </table>
     </div>
