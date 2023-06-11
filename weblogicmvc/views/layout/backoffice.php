f5316340af92c4af30db6bd71e0fc0bca46c31f8
RafaelMCoelho92 <114529694+RafaelMCoelho92@users.noreply.github.com>
2023-06-11 10:35:51 +0100

    cortar cenas a mais

--- weblogicmvc/views/layout/backoffice.php
+++ weblogicmvc/views/layout/backoffice.php
@@ -1,51 +1,7 @@
 <div class="container px-4 py-5" id="hanging-icons">
   <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
 
+    <!-- por aqui as cenas -->
 
-    <div class="col d-flex align-items-start">
-      <div class="icon-square text-body-emphasis bg-body-secondary d-inline-flex align-items-center justify-content-center fs-4 flex-shrink-0 me-3">
-        <svg class="bi" width="1em" height="1em">
-          <use xlink:href="#tools"></use>
-        </svg>
-      </div>
-      <div>
-        <h3 class="fs-2">Folhas de Obra</h3>
-        <p>Clique para criar uma nova Folha de Obra</p>
-        <a href="#" class="btn btn-primary">
-          Entrar
-        </a>
-      </div>
-
-    </div>
-    <div class="col d-flex align-items-start">
-      <div class="icon-square text-body-emphasis bg-body-secondary d-inline-flex align-items-center justify-content-center fs-4 flex-shrink-0 me-3">
-        <svg class="bi" width="1em" height="1em">
-          <use xlink:href="#tools"></use>
-        </svg>
-      </div>
-      <div>
-        <h3 class="fs-2">Serviços</h3>
-        <p>Clique para entrar nas definições de Serviço.</p>
-        <a href="index.php?c=service&a=index" class="btn btn-primary">
-          Entrar
-        </a>
-      </div>
-
-    </div>
-    <div class="col d-flex align-items-start">
-      <div class="icon-square text-body-emphasis bg-body-secondary d-inline-flex align-items-center justify-content-center fs-4 flex-shrink-0 me-3">
-        <svg class="bi" width="1em" height="1em">
-          <use xlink:href="#tools"></use>
-        </svg>
-      </div>
-      <div>
-        <h3 class="fs-2">Taxas de IVA</h3>
-        <p>Clique para entrar nas definições de IVA.</p>
-        <a href="index.php?c=iva&a=index" class="btn btn-primary">
-          Entrar
-        </a>
-      </div>
-
-    </div>
   </div>
 </div>
