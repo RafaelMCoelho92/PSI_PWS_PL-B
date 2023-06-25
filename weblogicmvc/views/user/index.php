d2ff67c6f5ec1923d3e500530bf333f4c22e0e54
RafaelMCoelho92 <114529694+RafaelMCoelho92@users.noreply.github.com>
2023-06-25 10:39:30 +0100

    search users

--- weblogicmvc/views/user/index.php
+++ weblogicmvc/views/user/index.php
@@ -3,23 +3,38 @@
 <div class="row">
     <div class="col-sm-12">
         <table class="table table-striped">
-            <thead>
-                <th>
-                    <h3>Nome</h3>
-                </th>
-                <th>
-                    <h3>Email</h3>
-                </th>
-                <th>
-                    <h3>NIF</h3>
-                </th>
-                <th>
-                    <h3>Role</h3>
-                </th>
-                <th>
-                    <a href='index.php?c=user&a=create' class="btn btn-success"> Criar Utilizador </a>
-                </th>
-            </thead>
+        <thead>
+        <tr>
+        <th colspan="4">
+            <div class="row">
+                <div class="col">
+                    <input type="text" id="searchInput" placeholder="Digite o termo de pesquisa" class="form-control">
+                </div>
+                <div class="col-auto">
+                    <button id="searchButton" class="btn btn-primary" role="button">Pesquisar</button>
+                </div>
+            </div>
+        </th>
+    </tr>
+    <tr>
+        <th>
+            <h3>Nome</h3>
+        </th>
+        <th>
+            <h3>Email</h3>
+        </th>
+        <th>
+            <h3>NIF</h3>
+        </th>
+        <th>
+            <h3>Role</h3>
+        </th>
+        <th>
+            <a href='index.php?c=user&a=create' class="btn btn-success">Criar Utilizador</a>
+        </th>
+    </tr>
+</thead>
+
             <tbody>
                 <?php foreach ($users as $user) { ?>
                     <tr>
@@ -39,3 +54,22 @@
         </table>
     </div>
 </div>
+<script>
+    document.getElementById('searchButton').addEventListener('click', function () {
+        var input = document.getElementById('searchInput').value.toLowerCase();
+        var rows = document.getElementsByTagName('tr');
+
+        for (var i = 2; i < rows.length; i++) {
+            var username = rows[i].getElementsByTagName('td')[0].textContent.toLowerCase();
+            var email = rows[i].getElementsByTagName('td')[1].textContent.toLowerCase();
+            var nif = rows[i].getElementsByTagName('td')[2].textContent.toLowerCase();
+            var role = rows[i].getElementsByTagName('td')[3].textContent.toLowerCase();
+
+            if (username.indexOf(input) > -1 || email.indexOf(input) > -1 || nif.indexOf(input) > -1 || role.indexOf(input) > -1) {
+                rows[i].style.display = '';
+            } else {
+                rows[i].style.display = 'none';
+            }
+        }
+    });
+</script>
