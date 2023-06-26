bb731778ac03c92989997be26305b786053a70b4
RafaelMCoelho92 <114529694+RafaelMCoelho92@users.noreply.github.com>
2023-06-26 14:25:38 +0100

    dados empresa

--- weblogicmvc/controllers/EmpresaController.php
+++ weblogicmvc/controllers/EmpresaController.php
@@ -6,7 +6,7 @@ class EmpresaController extends Controller // acesso aos métodos e propriedades
 {
     public function __construct()
     {
-        $this->authorizationFilter(['Funcionario', 'Admin']);
+        $this->authorizationFilter(['Funcionario', 'Admin', 'Cliente']);
         //Ele chama o método authorizationFilter(['Funcionario','Admin']) definido na classe Controller. 
         //Esse método verifica se o usuário está autenticado como "Funcionario" ou "Admin". 
         //Caso contrário, o usuário é redirecionado para uma rota de acesso inválido.
@@ -31,11 +31,13 @@ class EmpresaController extends Controller // acesso aos métodos e propriedades
     }
     public function create()
     {
+        $this->authorizationFilter(['Admin']);
         //mostra vista com form de criacao de registo
         $this->renderView('empresa', 'create');
     }
     public function store()
     {
+        $this->authorizationFilter(['Admin']);
         //recebe os dados do form de criacao valida e persiste na BD
         $empresa = new Empresa($this->getHTTPPost());
         if ($empresa->is_valid()) {
@@ -50,6 +52,7 @@ class EmpresaController extends Controller // acesso aos métodos e propriedades
     }
     public function edit($id)
     {
+        $this->authorizationFilter(['Admin']);
         //mostra a vista com form de edicao de um registo identificado pelo seu ID
         $empresa = Empresa::find($id);
         if (is_null($empresa)) {
@@ -61,6 +64,7 @@ class EmpresaController extends Controller // acesso aos métodos e propriedades
     }
     public function update($id)
     {
+        $this->authorizationFilter(['Admin']);
         // recebe os dados do form de edicao de um registo identificado pelo seu id valida e persiste na BD
         $empresa = Empresa::find($id);
         $empresa->update_attributes($this->getHTTPPost());
@@ -75,6 +79,7 @@ class EmpresaController extends Controller // acesso aos métodos e propriedades
     }
     public function delete($id)
     {
+        $this->authorizationFilter(['Admin']);
         //apaga um registo da BD identificado pelo ID
         $empresa = Empresa::find($id);
         $empresa->delete();
