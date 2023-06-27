4633454c0c0c99c62c92fb75de60cb5b9e90199f
AndreDuarte95 <2220894@my.ipleiria.pt>
2023-06-27 22:53:31 +0100

    dashboardfo, dashboardbo, frontoffice e default ok
    verificquem e vejam se concordam sff

--- weblogicmvc/views/home/dashboardfo.php
+++ weblogicmvc/views/home/dashboardfo.php
@@ -6,63 +6,64 @@
                 <!-- small box -->
                 <div class="small-box bg-info">
                     <div class="inner">
-                        <h3>3</h3>
+                        <h3><?= $numfolhasobras ?></h3>
 
                         <p>Folhas de Obra</p>
                     </div>
                     <div class="icon">
                         <i class="ion ion-bag"></i>
                     </div>
-                    <a href="index.php?c=folhaobra&a=index" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    <a href="index.php?c=cliente&a=folhaobraindex&id=<?= $auth->getId() ?>" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                 </div>
             </div>
+
             <!-- ./col -->
             <div class="col-lg-3 col-6">
                 <!-- small box -->
                 <div class="small-box bg-success">
                     <div class="inner">
-                        <h3><?= $numfolhasobras ?><sup style="font-size: 20px"></sup></h3>
+                        <h3><?= $numfolhasobraspagas ?><sup style="font-size: 20px"></sup></h3>
 
                         <p>Folhas de Obra Pagas</p>
                     </div>
                     <div class="icon">
                         <i class="ion ion-stats-bars"></i>
                     </div>
-                    <a href="index.php?c=&a=" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    <a href="index.php?c=cliente&a=folhaobrapaga&id=<?= $auth->getId() ?>" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                 </div>
             </div>
+
             <!-- ./col -->
             <div class="col-lg-3 col-6">
                 <!-- small box -->
                 <div class="small-box bg-warning">
                     <div class="inner">
-                        <h3><?= $numusers ?></h3>
-
-                        <p>Folhas de Obra em Pagamento</p>
+                        <h3><?= $numfolhasobrasemitidas ?></h3>
+                        <p>N.º Folhas de Obras Emitidas</p>
                     </div>
                     <div class="icon">
                         <i class="ion ion-person-add"></i>
                     </div>
-                    <a href="index.php?c=&a=" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    <a href="index.php?c=cliente&a=folhaobraemitida&id=<?= $auth->getId() ?>" class="small-box-footer">Ver Emitidas <i class="fas fa-arrow-circle-right"></i></a>
                 </div>
             </div>
+
             <!-- ./col -->
             <div class="col-lg-3 col-6">
                 <!-- small box -->
                 <div class="small-box bg-danger">
                     <div class="inner">
-                        <h3>5</h3>
+                        <h4> <strong>Área Pessoal</strong></h4>
 
-                        <p>Área Pessoal</p>
+                        <p>Alterar Dados</p>
                     </div>
                     <div class="icon">
                         <i class="ion ion-pie-graph"></i>
                     </div>
-                    <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    <a href="index.php?c=cliente&a=edit" <?= $auth->getId() ?> class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                 </div>
             </div>
-            <!-- ./col -->
         </div>
 
-    </div><!-- /.container-fluid -->
+    </div>
 </section>
