e6f2c21effe898737e1aaa87d863375ca9ada7b9
RafaelMCoelho92 <114529694+RafaelMCoelho92@users.noreply.github.com>
2023-06-29 01:15:25 +0100

    linhaobra quantidade > 0

--- weblogicmvc/models/Linhaobra.php
+++ weblogicmvc/models/Linhaobra.php
@@ -6,4 +6,7 @@ class Linhaobra extends \ActiveRecord\Model
         array('folhaobra', 'class_name' => 'Folhaobra', 'foreign_key' => 'idfolhaobra'),
         array('servico', 'class_name' => 'Service', 'foreign_key' => 'idservico')
     );
+    static $validates_numericality_of = array(
+        array('quantidade', 'greater_than_or_equal_to' => 0,'message' => 'O valor tem de ser superior ou igual a 0.') // penso n fazer sentido ser negativo
+    );
 }
