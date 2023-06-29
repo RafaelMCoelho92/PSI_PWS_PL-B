064f03aed14f0683c9b9974cabe7384f183b95dd
AndreDuarte95 <2220894@my.ipleiria.pt>
2023-06-29 01:58:42 +0100

    APP_NAME ja aparece no index login

--- weblogicmvc/views/home/index.php
+++ weblogicmvc/views/home/index.php
@@ -2,7 +2,7 @@
   <div class="row align-items-center g-lg-5 py-5">
     <div class="col-lg-7 text-center text-lg-start">
       <h1 class="display-4 fw-bold lh-1 text-body-emphasis mb-3">
-        </?= APP_NAME ?>
+        <?= APP_NAME ?>
       </h1>
       <p class="col-lg-10 fs-4">Faça login para entrar na aplicação.</p>
     </div>
